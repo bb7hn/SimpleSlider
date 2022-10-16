f58846a535c011c902b04e67d373716163e3f27d
Batuhan OZEN <iletisim@batuhanozen.com>
2022-10-16 14:27:44 +0100

    Cancelled drag events
    In this branch drag events cancelled and added touch listeners for mobile

--- index.js
+++ index.js
@@ -1,82 +1,98 @@
 "use strict"
 const defaultProps = {
     container: null,
-    imgList : [],
-    controls:{
-        next:false,
-        prev:false
+    imgList: [],
+    controls: {
+        next: false,
+        prev: false
     },
     autoPlay: true,
 };
-class simpleSlider{
+class simpleSlider {
+
+    constructor(){
+        this.dragStartX = 0;
+        this.touches = [];
+    }
     
-    init (opts){
+    init(opts) {
         // set default options
         const options = { ...defaultProps, ...opts }
         // Set options of class
         this.setOptions(options);
         // Check did root element set by user
-        if(typeof options.container === null){
+        if (typeof options.container === null) {
             console.error('SimpleSliderJS : "root" option is needed and have to be html element');
         }
         // Get image list from options
         const { imgList } = options;
         // Check imgList if it's length === 0 or it's type is not array throw error 
-        if(typeof imgList !== typeof[] || imgList.length === 0){
+        if (typeof imgList !== typeof [] || imgList.length === 0) {
             console.error('SimpleSliderJS : "imgList" option does not contain any url (need to be array of strings)');
             return;
         }
         // TODO check item types of array accept only if they are strings or html elements
-        
+
         // if user did not set custom controls create default controls
-        if(options.controls.next === false || options.controls.prev !== false){
+        if (options.controls.next === false || options.controls.prev !== false) {
             this.createControls();
         }
 
         this.slideCount = imgList.length;
 
         this.createImages(imgList);
-        
+
         this.setActiveNavItem();
         // check did user set autoplay
         this.autoPlay();
 
+        /* this.root.addEventListener("dragstart", this.dragStartHandler); */
+        /* this.root.addEventListener("dragend", this.dragEndHandler); */
+
+        this.root.addEventListener("touchstart", this.touchStartHandler);
+        this.root.addEventListener("touchend", this.touchEndHandler);
+        this.root.addEventListener('touchmove', this.touchMoveHandler);
+        this.root.addEventListener('touchleave', this.touchLeaveHandler);
+        this.root.addEventListener('touchcancel', this.touchCancelHandler);
+
+        this.root.simpleSlider = this;
         return this;
     }
 
-    setOptions(options){
+    setOptions(options) {
         this.options = options;
         this.root = options.container;
         this.currentSlide = 0;
     }
 
-    createImages(urlList){
+    createImages(urlList) {
         urlList.forEach((url, i) => {
             const imgContainer = document.createElement('div');
             imgContainer.style.transition = "all 400ms";
             imgContainer.style.width = "100%";
             imgContainer.style.display = "flex";
+            imgContainer.style.position = "relative";
             const img = document.createElement('img');
             img.src = url;
-            
-            imgContainer.appendChild(img);
+            img.draggable = false;
+            imgContainer.append(img);
             this.root.append(imgContainer);
         });
     }
 
-    createControls(){
+    createControls() {
         const previousButton = document.createElement('div');
-        previousButton.setAttribute('data-type','control');
+        previousButton.setAttribute('data-type', 'control');
         previousButton.style.zIndex = "2";
         previousButton.innerHTML = "&#8249;";
         previousButton.style.width = "4rem";
         previousButton.style.height = "4rem";
         previousButton.style.color = "skyblue";
         previousButton.style.borderRadius = "100%";
-        previousButton.style.position="absolute";
+        previousButton.style.position = "absolute";
         previousButton.style.fontSize = "3rem";
         previousButton.style.top = "50%";
-        previousButton.style.transform="translateY(-50%)";
+        previousButton.style.transform = "translateY(-50%)";
         previousButton.style.left = "0";
         previousButton.style.display = "flex";
         previousButton.style.alignItems = "center";
@@ -86,17 +102,17 @@ class simpleSlider{
         previousButton.onclick = () => this.goToSlide('<');
 
         const nextButton = document.createElement('div');
-        nextButton.setAttribute('data-type','control');
+        nextButton.setAttribute('data-type', 'control');
         nextButton.style.zIndex = "2";
         nextButton.innerHTML = "&#8250;";
         nextButton.style.width = "4rem";
         nextButton.style.height = "4rem";
         nextButton.style.color = "skyblue";
         nextButton.style.borderRadius = "100%";
-        nextButton.style.position="absolute";
+        nextButton.style.position = "absolute";
         nextButton.style.fontSize = "3rem";
         nextButton.style.top = "50%";
-        nextButton.style.transform="translateY(-50%)";
+        nextButton.style.transform = "translateY(-50%)";
         nextButton.style.right = "0";
         nextButton.style.display = "flex";
         nextButton.style.alignItems = "center";
@@ -106,7 +122,7 @@ class simpleSlider{
         nextButton.onclick = () => this.goToSlide('>');
 
         const bottomNavigation = document.createElement('div');
-        bottomNavigation.setAttribute('data-type','control');
+        bottomNavigation.setAttribute('data-type', 'control');
         bottomNavigation.style.zIndex = "2";
         bottomNavigation.style.padding = "10px";
         bottomNavigation.style.display = "flex";
@@ -117,7 +133,7 @@ class simpleSlider{
         bottomNavigation.style.bottom = "0";
         bottomNavigation.style.transform = "translateX(-50%)";
         bottomNavigation.style.userSelect = "none";
-        this.options.imgList.forEach((_,index)=>{
+        this.options.imgList.forEach((_, index) => {
             const circle = document.createElement('span');
             circle.style.width = "20px";
             circle.style.height = "20px";
@@ -133,23 +149,23 @@ class simpleSlider{
 
         this.bottomNav = bottomNavigation.children;
 
-        this.root.append(previousButton,nextButton,bottomNavigation);
+        this.root.append(previousButton, nextButton, bottomNavigation);
     }
 
-    goToSlide(index,event = "userClick"){
-        if(index === '<'){
+    goToSlide(index, event = "userClick") {
+        if (index === '<') {
             // go to previous
-            if(this.currentSlide > 0){
+            if (this.currentSlide > 0) {
                 // if selected slide bigger than 0 go back else go to last one
                 this.setActiveSlide(-1);
             }
-            else{
+            else {
                 this.currentSlide = this.slideCount;
                 this.goToSlide('<');
             }
-        } else if(index === '>'){
+        } else if (index === '>') {
             // go to next
-            if(this.currentSlide < this.slideCount - 1){
+            if (this.currentSlide < this.slideCount - 1) {
                 // if selected slide smaller than total slide count minus one go forward else go to first one
                 this.setActiveSlide();
             } else {
@@ -157,49 +173,104 @@ class simpleSlider{
                 this.goToSlide('>');
             }
         }
-        else{
+        else {
             // parseint index and go to index
             console.log(parseInt(index));
             this.currentSlide = index;
             this.setActiveSlide(0);
         }
-        if(event === "userClick"){
+        if (event === "userClick") {
             this.autoPlay();
         }
     }
 
-    setActiveNavItem(){
-        Array.from(this.bottomNav).forEach(item=>{
+    setActiveNavItem() {
+        Array.from(this.bottomNav).forEach(item => {
             item.style.background = "#FFFFFF50";
         });
         this.bottomNav[this.currentSlide].style.background = "#FFFFFF90";
     }
 
-    setActiveSlide(multiplier=1){
-        const slides  = this.root.querySelectorAll('div:not([data-type="control"])');
+    setActiveSlide(multiplier = 1) {
+        const slides = this.root.querySelectorAll('div:not([data-type="control"])');
         const slideWidth = slides[0].getBoundingClientRect().width;
 
         slides.forEach((slide) => {
-            slide.style.transform = `translateX(-${slideWidth * (this.currentSlide+multiplier)}px)`; 
+            slide.style.transform = `translateX(-${slideWidth * (this.currentSlide + multiplier)}px)`;
         });
 
         this.currentSlide += multiplier;
         this.changedCB(this.currentSlide);
         this.setActiveNavItem();
     }
-    on(type, callBack){
+    on(type, callBack) {
         this[`${type}CB`] = callBack;
     }
 
-    autoPlay(){
+    autoPlay() {
         const options = this.options;
-        if(options.autoPlay === true){
-            if(this.autoPlayInterval){
+        if (options.autoPlay === true) {
+            if (this.autoPlayInterval) {
                 clearInterval(this.autoPlayInterval);
             }
             this.autoPlayInterval = setInterval(() => {
-                this.goToSlide('>',"autoPlay");
+                this.goToSlide('>', "autoPlay");
             }, options.autoPlayDuration || 4000);
+        }
+    }
+
+/*     dragStartHandler(e) {
+        const dragImg = new Image();
+        dragImg.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+        e.dataTransfer.setDragImage(dragImg, 0, 0);
+        const { screenX } = e;
+        const simpleSlider = this.simpleSlider;
+        simpleSlider.dragStartX = screenX;
+        console.log('dragging',simpleSlider);
+        //console.log('clientX => ', clientX);
+        //console.log('clientY => ', clientY);
+        //console.log('layerX => ', layerX);
+        //console.log('layerY => ', layerY);
+        //console.log('offsetX => ', offsetX);
+        //console.log('offsetY => ', offsetY);
+        //console.log('pageX => ', pageX);
+        //console.log('pageY => ', pageY);
+        //console.log('screenX => ', screenX);
+        //console.log('screenY => ', screenY);
+    }
+    dragEndHandler(e) {
+        const { screenX } = e;
+        e.preventDefault();
+        const simpleSlider = this.simpleSlider;
+        console.log('dragged',simpleSlider);
+        if(screenX > simpleSlider.dragStartX){
+            simpleSlider.goToSlide('<');
+        }
+        else{
+            simpleSlider.goToSlide('>');
+        }
+
+        simpleSlider.dragStartX = 0;
+    } */
+
+    touchStartHandler(e) {
+        //set x starting location in here
+        this.simpleSlider.touches[0] = e.touches[0].screenX;
+    }
+    touchMoveHandler(e){
+        // set x location in here
+        this.simpleSlider.touches[1] = e.touches[e.touches.length - 1].screenX;
+    }
+    touchEndHandler(e) {
+        // handle where to go in here (prev or next)
+        const simpleSlider = this.simpleSlider;
+        const firstTouch = simpleSlider.touches[0];
+        const lastTouch = simpleSlider.touches[1];
+        if(lastTouch > firstTouch){
+            simpleSlider.goToSlide('<');
+        }
+        if(lastTouch < firstTouch){
+            simpleSlider.goToSlide('>');
         }
     }
 }
